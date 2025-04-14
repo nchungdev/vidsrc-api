86a74925a984f9eeb43c0e7f9850d8ce3637247f
chungnh <chungnh@vng.com.vn>
2025-04-14 10:54:41 +0700

    Config support for vercel

---
+++ src/app/movie/[id]/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { getMovie } from '@/app/api';
+
+export async function GET(request, { params }) {
+  const { id } = await params;
+	return NextResponse.json(await getMovie(id));
+}
---
+++ src/app/tv/[id]/[season]/[episode]/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import { getMovie, getTv } from '@/app/api';
+
+export async function GET(req, { params }) {
+	const { id, season, episode } = params;
+	if (isNaN(parseInt(id))) {
+		return NextResponse.json({ error: 'Invalid tv id' });
+	}
+	if (isNaN(parseInt(season))) {
+		return NextResponse.json({ error: 'Invalid season number' });
+	}
+	if (isNaN(parseInt(episode))) {
+		return NextResponse.json({ error: 'Invalid episode number' });
+	}
+	const output = await getTv(id, season, episode);
+	return NextResponse.json(output);
+}
